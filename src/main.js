c111b50013be5858f43810061a0efe9873100adb
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-04-05 11:00:39 +0200

    feat: initialize Leaflet map with OpenStreetMap tiles and default marker
    - Created `getMap` function to initialize a Leaflet map centered at default coordinates [51.505, -0.09] with zoom level 13
    - Added OpenStreetMap tile layer with attribution and maxZoom set to 19
    - Placed a marker at default coordinates [51.5, -0.09]
    - Invoked `getMap()` to render the map on page load

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import "./style.css";
 import viteLogo from "/vite.svg";
+
+const getMap = () => {
+  const map = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const marker = L.marker([51.5, -0.09]).addTo(map);
+};
+
+getMap();
