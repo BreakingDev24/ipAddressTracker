66f01ed9069e0a3512d90cd8d9ebbf51f4de211d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-04-08 12:23:34 +0200

    feat(location): enhance map functionality and input handling
    - Refactored getMap function to manage the map and markers more effectively.
    - Added input type validation for IP address and domain in getInputType function.
    - Improved getLocation function to handle invalid input and construct the API URL based on input type.
    - Integrated form submission handling to fetch location based on user input.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import "./style.css";
 import viteLogo from "/vite.svg";
 
@@ -6,21 +8,49 @@ const location = document.querySelector(".location_text");
 const timezoneText = document.querySelector(".timezone_text");
 const ispText = document.querySelector(".isp_text");
 
-const getMap = (lat, lng) => {
-  const map = L.map("map").setView([lat, lng], 13);
+const ipForm = document.querySelector(".ipForm");
+const formInput = document.querySelector(".formInput");
 
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+let map;
+let currentMarker;
 
-  const marker = L.marker([lat, lng]).addTo(map);
+const getInputType = (input) => {
+  const ipv4Regex =
+    /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/;
+  const ipv6Regex = /^(([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}|::1|::)$/;
+  const domainRegex =
+    /^(?!:\/\/)(?=.{1,253}$)(([a-zA-Z0-9-_]{1,63}\.)+[a-zA-Z]{2,63})$/;
+
+  if (ipv4Regex.test(input) || ipv6Regex.test(input)) return "ipAddress";
+  if (domainRegex.test(input)) return "domain";
+  return "invalid";
 };
 
-const getLocation = async () => {
+const getMap = (lat, lng) => {
+  if (!map) {
+    map = L.map("map").setView([lat, lng], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+  } else {
+    map.setView([lat, lng], 13);
+  }
+
+  if (currentMarker) map.removeLayer(currentMarker);
+  currentMarker = L.marker([lat, lng]).addTo(map);
+};
+
+const getLocation = async (input = "") => {
   const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`;
+  const type = getInputType(input);
+  if (input !== "" && type === "invalid") return;
+
+  const url =
+    `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}` +
+    (input && `&${type}=${input}`);
   try {
     const resGeo = await fetch(url);
     if (!resGeo.ok) throw new Error(`HTTP error! Status: ${response.status}`);
@@ -49,5 +79,12 @@ const showData = (ip, isp, timezone, city, country) => {
 // console.log(import.meta.env.VITE_API_KEY);
 
 getLocation();
+
+ipForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputText = formInput.value;
+  formInput.value = "";
+  getLocation(inputText);
+});
 
 // getMap();
